bc2ebe34f543194820be84c2149b66cdeeb30268
tumolo <gregt@frankdeals.com>
2011-08-17 14:19:07 -0400

    New blog repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '51089c73e8c145ad1c039808b936f26365af4e2f57910ed2a04eff3553be6e9ea1bdfa173d09d9158535da97e459cf87952db7eaec25ae8ff2f27979e34be6da'
